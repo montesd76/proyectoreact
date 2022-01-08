7cdc500062c353c7ec749018460d1493f5415f2c
Diego E Montes <DEMontes@teco.com.ar>
2022-01-08 18:14:05 -0300

    version 1.0

--- src/App.js
+++ src/App.js
@@ -6,8 +6,10 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        
+        <h1>Diego Montes</h1>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Clase 2
         </p>
         <a
           className="App-link"
